3b53a6bece43f45188f391816028bcbe0d1130f8
Jagankumarg <jagan0523@gmail.com>
2023-10-06 14:02:21 +0530

    pagination and sorting

--- src/main/java/com/paginaton/sorting/repository/EmployeeRepository.java
+++ src/main/java/com/paginaton/sorting/repository/EmployeeRepository.java
@@ -2,6 +2,7 @@ package com.paginaton.sorting.repository;
 
 import com.paginaton.sorting.dto.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
+public interface EmployeeRepository extends JpaRepository<Employee,Integer>, PagingAndSortingRepository<Employee,Integer> {
 }
