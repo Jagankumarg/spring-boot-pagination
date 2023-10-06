fd585b18a1aecf45f9d5ef2e3829a4ec02631765
Jagankumarg <jagan0523@gmail.com>
2023-10-06 11:02:26 +0530

    java7 data load, H2 configuration

--- src/main/java/com/paginaton/sorting/controller/PaginationHealthCheckController.java
+++ src/main/java/com/paginaton/sorting/controller/PaginationHealthCheckController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class PaginationHealthCheckController {
     @GetMapping("/pagination/healthCheck")
-    public String healthCheck(){
+    public String healthCheck() {
         return "Pagination Application is up and running";
     }
 }
