e9c13b22de534d901439660c3a09c0257ba34abd
Jagankumarg <jagan0523@gmail.com>
2023-10-06 10:05:26 +0530

    Pagination Initial commit

---
+++ src/main/java/com/paginaton/sorting/SpringBootPaginationApplication.java
@@ -0,0 +1,13 @@
+package com.paginaton.sorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPaginationApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/paginaton/sorting/controller/PaginationHealthCheckController.java
@@ -0,0 +1,13 @@
+package com.paginaton.sorting.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class PaginationHealthCheckController {
+    @GetMapping("/pagination/healthCheck")
+    public String healthCheck(){
+        return "Pagination Application is up and running";
+    }
+}
